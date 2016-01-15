2347720f149a264a393da7eab4d9eb1ed256253c
Nancy Granda <nancy.granda.d@hotmail.com>
2016-01-15 13:40:59 -0600

    added empty list test

--- simple_stat_script.py
+++ simple_stat_script.py
@@ -1,15 +1,23 @@
 def mean(vals):
    '''Calculate the arithmetic mean of a list'''
    assert type(vals) is list, 'wrong input'
-
-   total = sum(vals)
+   
    length = len(vals)
-   return total/length
+   total = sum(vals)
+
+   if length  == 0.0:
+      return 0.0
+   else:
+      return total/length
 
 def test_mean():
    assert mean([2,4]) == 3.0
 
 test_mean()
+
+def test_empty_list():
+   assert mean([]) == 0.0
+test_empty_list()
 
 #print (mean([2,4])) 
 #print (mean('hello'))
