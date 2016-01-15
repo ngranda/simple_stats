4582c9cd5e650487f087cd52c7de82908623e11a
Nancy Granda <nancy.granda.d@hotmail.com>
2016-01-15 14:06:57 -0600

    added some nosetest tools

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,14 @@
 from stats import mean 
+from nose.tools import assert_equal, assert_almost_equals
 
 def test_mean():
-   assert mean([2,4]) == 3.0
-test_mean()
+   assert_equal(mean([2,4]),3.0)
+#test_mean()
 
 def test_empty_list():
-   assert mean([]) == 0.0
-test_empty_list()
+   assert_equal(mean([]), 0.0)
+#test_empty_list()
 
 def test_float_mean():
-   assert mean ([0.5,0.5,1]) -- 0.666
-test_float_mean()
+   assert_almost_equals(mean([0.5,0.5,1]), 0.6666, 2)
+#test_float_mean()
