340662d153f4c1ec385dc8a0843db54a9bd50c00
Nancy Granda <nancy.granda.d@hotmail.com>
2016-01-15 13:27:50 -0600

    added a test function

--- simple_stat_script.py
+++ simple_stat_script.py
@@ -1,10 +1,15 @@
 def mean(vals):
-#"""Calculate the arithmetic mean of a list"""
+   '''Calculate the arithmetic mean of a list'''
    assert type(vals) is list, 'wrong input'
 
    total = sum(vals)
    length = len(vals)
    return total/length
 
+def test_mean():
+   assert mean([2,4]) == 3.0
+
+test_mean()
+
 #print (mean([2,4])) 
-print (mean("hello"))
+#print (mean('hello'))
